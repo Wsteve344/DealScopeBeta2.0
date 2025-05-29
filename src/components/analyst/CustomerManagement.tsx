c01eb38e5d214cc1e6180c60272c98f3434912b2
Wsteve344 <stevenscapitol@gmail.com>
2025-05-29 18:19:04 -0400

    Create Customer Signup Report Component

--- src/components/analyst/CustomerManagement.tsx
+++ src/components/analyst/CustomerManagement.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import CustomerSignupReport from './CustomerSignupReport';
 import { 
   Search, UserCog, CreditCard, Trash2, RefreshCw, Download, 
   AlertCircle, CheckCircle, History, Plus, Minus, FileText
